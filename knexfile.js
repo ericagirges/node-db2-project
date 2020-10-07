e477059be6886ee51609473aa35cf8c335a29eed
Erica Girges <ericafiorenza@gmail.com>
2020-10-06 20:50:43 -0700

    further set up

--- knexfile.js
+++ knexfile.js
@@ -5,25 +5,32 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/cars.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+  },
+    seeds: {
+      directory: './database/seeds'
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
     client: 'postgresql',
